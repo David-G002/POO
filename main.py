f3b5fb863ee1b2a380edb474e8b7f7ca7d99c9f9
David-G002 <david.godoy18@inacapmail.cl>
2026-09-21 16:57:19 -0300

    avanze main

--- main.py
+++ main.py
@@ -1,9 +1,73 @@
 from paciente import Paciente 
+pacientes:list[Paciente]=[]
 
+def leer_numero(mensaje:str)->int:
+    while True:
+        try:
+            numero=int(input(mensaje))
+            return numero 
+        except ValueError:
+            print("Error: Debe ingresar un numero entero")
+
+def menu():
+    print("="*20)
+    print("Menu clinica")
+    print("="*20)
+    print("1.- Agregar paciente")
+    print("2.- Editar paciente")
+    print("3.- Eliminar paciente")
+    print("4.- Mostrar un paciente")
+    print("5.- Mostrar todos los pacientes")
+    print("0.- Salir")
+    op =leer_numero("Ingrese una opcion: ")
+    print("opcion seleccionada: ",op)
+    print("="*20)
+    return op 
+def agregar_paciente()->None:
+    rut=input("Ingrese RUT del paciente: ")
+    nombre=input("Ingrese el nombre del paciente: ")
+    edad=leer_numero("Ingrese edad del paciente: ")
+    print("Tipo de prevision del paciente: ")
+    print("1.- Fonasa")
+    print("2.- Isapre")
+    print("3.- Particular")
+    print("4.- Otro")
+    op=leer_numero("Seleccione una prevision del paciente: ")
+    if op==1:
+        prevision="Fonasa"
+    elif op==2:
+        prevision="Isapre"
+    elif op==3:
+        prevision="Particular"
+    elif op==4:
+        prvision="Otro"
+
+    paciente=Paciente(rut,nombre,edad,prevision)
+    pacientes.append(paciente)
+    print("Paciente agregado exitosamente")
+    print(f"Total de pacientes: {len(pacientes)}")
+    
 def main():
-    #creando objeto paciente
-    p1=Paciente("11.111.111-1", "La roca", 50,"Fonasa")
-    print(p1) 
+    while True:
+        opcion=menu()
+        if opcion==1:
+            print("Agregar paciente")
+            agregar_paciente()
+        elif opcion==2:
+            print("Editar paciente")
+        elif opcion==3:
+            print("Eliminar paciente")
+        elif opcion==4:
+            print("Mostrar un paciente")
+        elif opcion==5:
+            print("Mostrar todos los pacientes")
+        elif opcion==0:
+            print("Saliendo del programa...")
+            break 
+        else:
+            print("Opcion invalida, Intente nuevamente")
+
+
 
 
 if __name__=="__main__":
